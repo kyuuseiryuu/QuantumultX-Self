bfd7c71b4623e4d57f5399dcc1115603c3916d3e
KSR <KSR@local.lan>
2020-10-29 18:39:25 +0800

    update

--- fzdm/scripts/rewrite_ad_jsonp.js
+++ fzdm/scripts/rewrite_ad_jsonp.js
@@ -1,6 +1,8 @@
 const contentJS = `
 try {
   document.querySelector('#xuanfu_news_id').remove();
+} catch() {}
+try {
   document.querySelector('[data-type="close111"]').click();
 } catch() {}
 document.querySelectorAll('newsfeed').forEach(e => e.remove());
