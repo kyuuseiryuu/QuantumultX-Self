8b89e97efc1d47052e4af278d608c4922d336e0c
KSR <KSR@local.lan>
2020-10-29 22:50:05 +0800

    update

--- fzdm/scripts/rewrite_360_ad.js
+++ fzdm/scripts/rewrite_360_ad.js
@@ -1,5 +1,5 @@
 const contentJS = `
-document.querySelectorAll('#xuanfu_news_id')forEach(e => e.remove());
+document.querySelectorAll('#xuanfu_news_id').forEach(e => e.remove());
 document.querySelectorAll('newsfeed').forEach(e => e.remove());
 document.querySelectorAll('center').forEach(e => e.remove());
 document.querySelectorAll('cmx').forEach(e => e.remove());
