c1c3f64dbbc22f62580fbc2173f2e6f3efacfa3e
KSR <KSR@local.lan>
2020-10-29 16:24:26 +0800

    init

---
+++ fzdm/scripts/rewrite_360_cn_mlist.js
@@ -0,0 +1,7 @@
+const contentJS = `
+document.querySelector('#xuanfu_news_id').remove();
+document.querySelectorAll('newsfeed').forEach(e => e.remove());
+document.querySelectorAll('center').forEach(e => e.remove());
+`;
+
+$done({ body: contentJS });
