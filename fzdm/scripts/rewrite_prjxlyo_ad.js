2dc76bc308eab487e9e48b91c6cdc96f61d75ff1
KSR <KSR@local.lan>
2020-10-29 22:44:32 +0800

    update fzdm

--- fzdm/scripts/rewrite_prjxlyo_ad.js
+++ fzdm/scripts/rewrite_prjxlyo_ad.js
@@ -1,13 +1,7 @@
 const body = `
-setTimeout(() => {
-  const id = setInterval(() => {}, 1000);
-  for (let i = 0; i <= id; i++ ) {
-      clearInterval(i);
-  }
-  document.body.style.paddingTop = "0px";
-  const children = document.body.children;
-  children[children.length - 1].remove();
-  document.querySelectorAll('center').forEach(e => e.remove());
-}, 750);
+setInterval(() => {
+  document.querySelectorAll("[data-type='close111']").forEach(e => e.click());
+});
+document.querySelectorAll('center').forEach(e => e.remove());
 `
 $done({ body });
