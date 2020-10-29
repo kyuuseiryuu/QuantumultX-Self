98595cbf3456fdf80ef4f743efc3086528500bb7
KSR <KSR@local.lan>
2020-10-29 21:42:15 +0800

    update

--- fzdm/scripts/rewrite_prjxlyo_ad.js
+++ fzdm/scripts/rewrite_prjxlyo_ad.js
@@ -1,11 +1,13 @@
 const body = `
-const id = setInterval(() => {}, 1000);
-for (let i = 0; i <= id; i++ ) {
-    clearInterval(i);
-}
-document.body.style.paddingTop = "0px";
-const children = document.body.children;
-children[children.length - 1].remove();
-document.querySelectorAll('center').forEach(e => e.remove());
+setTimeout(() => {
+  const id = setInterval(() => {}, 1000);
+  for (let i = 0; i <= id; i++ ) {
+      clearInterval(i);
+  }
+  document.body.style.paddingTop = "0px";
+  const children = document.body.children;
+  children[children.length - 1].remove();
+  document.querySelectorAll('center').forEach(e => e.remove());
+}, 750);
 `
 $done({ body });
