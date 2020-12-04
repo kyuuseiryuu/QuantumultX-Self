5094dccc6a9d1cf9803ffceaaa44e7d3de232204
kyuu <kyuuseiryuu@kyuudeMacBook-Pro.local>
2020-12-04 18:56:02 +0800

    update

--- fzdm/scripts/rewrite_index_html.js
+++ fzdm/scripts/rewrite_index_html.js
@@ -1,12 +1,11 @@
 const content = `
-<script>
-  setInterval(() => {
+  <script>
+    setInterval(() => {
       document.querySelectorAll('[data-type="close111"]').forEach(e => e.click());
-  });
-</script>
+    });
+  </script>
+</html>
 `
-const body = $response.body.replace('</html>', '') + content + '</html>';
+const body = $response.body.replace('</html>', content);
 
 $done({ body });
-
-
